62930c9f35b280cf3bcfc49f56ad4128d2088934
Hatim <hatimhasbullah@gmail.com>
2025-06-02 12:43:39 +0800

    used Handle method of ServeMux to /static to serve index.html

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func main() {
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
+
+	// Use Handle method of serveMux
+	serveMux.Handle("/", http.FileServer(http.Dir("./static")))
+
 	log.Fatal(server.ListenAndServe())
 }
